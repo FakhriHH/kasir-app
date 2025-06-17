b1269072d4781a5dcd43f48e6cd11157f8df91d6
FakhriHH <fakhrihabib9@gmail.com>
2025-06-17 17:05:19 +0700

    Installasi knex

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,6 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err.message);
-    process.exit(1);
-  }
-  console.log("Connected to MySQL database");
-});
+const db = knex(knexConfig.development);
 
 module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
 require("dotenv").config();
-require("./config/db");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,12 +10,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+db.raw("SELECT 1")
+  .then(() => {
+    console.log("Database connected");
+    // Jalankan server hanya jika koneksi DB berhasil
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err.message);
+    process.exit(1);
+  });
+
 app.get("/", (req, res) => {
   res.send("Kasir Backend API is running 🚀");
 });
 
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
